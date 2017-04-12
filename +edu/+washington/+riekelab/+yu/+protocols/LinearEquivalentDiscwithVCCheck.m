8e3638ffd58d021d53c0b27984af2b8f052b1089
sadahanu <yuz04ster@gmail.com>
2017-04-12 14:39:39 -0700

    add a vc check - to be completed

---
+++ +edu/+washington/+riekelab/+yu/+protocols/LinearEquivalentDiscwithVCCheck.m
@@ -0,0 +1,260 @@
+classdef LinearEquivalentDiscwithVCCheck < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+% as before merging into NIF super class from turner Package
+% check contrast responses periodically
+    properties
+        preTime = 200 % ms
+        stimTime = 200 % ms
+        tailTime = 200 % ms
+        
+        imageName = '00152' %van hateren image names
+        seed = 1 % rand seed for picking image patches
+        noPatche_interval = 30 % after these image patches test discs will be shown
+        apertureDiameter = 200 % um
+        linearIntegrationFunction = 'gaussian center'
+        patchSampling = 'random'
+        patchContrast = 'all'
+
+        rfSigmaCenter = 50 % (um) Enter from fit RF
+
+        centerOffset = [0, 0] % [x,y] (um)
+        onlineAnalysis = 'none'
+        numberOfAverages = uint16(180) % number of epochs to queue
+        amp % Output amplifier
+    end
+    
+    properties (Hidden)
+        ampType
+        imageNameType = symphonyui.core.PropertyType('char', 'row', {'00152','00377','00405','00459','00657','01151','01154',...
+            '01192','01769','01829','02265','02281','02733','02999','03093',...
+            '03347','03447','03584','03758','03760'})
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        patchSamplingType = symphonyui.core.PropertyType('char', 'row', {'random','ranked'})
+        patchContrastType = symphonyui.core.PropertyType('char', 'row', {'all','negative','positive'})
+        linearIntegrationFunctionType = symphonyui.core.PropertyType('char', 'row', {'gaussian center','uniform'})
+        centerOffsetType = symphonyui.core.PropertyType('denserealdouble', 'matrix')
+        
+        wholeImageMatrix
+        imagePatchMatrix
+        allEquivalentIntensityValues
+        patchLocations
+        
+        %saved out to each epoch...
+        currentStimSet
+        backgroundIntensity
+        imagePatchIndex
+        currentPatchLocation
+        equivalentIntensity
+        stimulusTag
+    end
+
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,...
+                'groupBy',{'stimulusTag'});
+            obj.showFigure('edu.washington.riekelab.turner.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+            if ~strcmp(obj.onlineAnalysis,'none')
+                obj.showFigure('edu.washington.riekelab.turner.figures.ImageVsIntensityFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,...
+                'preTime',obj.preTime,'stimTime',obj.stimTime);
+            end
+            
+            %load appropriate image...
+            resourcesDir = 'C:\Users\Public\Documents\turner-package\resources\';
+            obj.currentStimSet = '/VHsubsample_20160105';
+            fileId=fopen([resourcesDir, obj.currentStimSet, '/imk', obj.imageName,'.iml'],'rb','ieee-be');
+            img = fread(fileId, [1536,1024], 'uint16');
+           
+            img = double(img);
+            img = (img./max(img(:))); %rescale s.t. brightest point is maximum monitor level
+            obj.backgroundIntensity = mean(img(:));%set the mean to the mean over the image
+            contrastImage = (img - obj.backgroundIntensity) ./ obj.backgroundIntensity;
+            img = img.*255; %rescale s.t. brightest point is maximum monitor level
+            obj.wholeImageMatrix = uint8(img);
+            rng(obj.seed); %set random seed for fixation draw
+            
+            %size of the stimulus on the prep:
+            stimSize = obj.rig.getDevice('Stage').getCanvasSize() .* ...
+                obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'); %um
+            stimSize_VHpix = stimSize ./ (3.3); %um / (um/pixel) -> pixel
+            radX = round(stimSize_VHpix(1) / 2); %boundaries for fixation draws depend on stimulus size
+            radY = round(stimSize_VHpix(2) / 2);
+            
+            %get patch locations:
+            load([resourcesDir,'NaturalImageFlashLibrary_072216.mat']);
+            fieldName = ['imk', obj.imageName];
+            %1) restrict to desired patch contrast:
+            LnResp = imageData.(fieldName).LnModelResponse;
+            if strcmp(obj.patchContrast,'all')
+                inds = 1:length(LnResp);
+            elseif strcmp(obj.patchContrast,'positive')
+                inds = find(LnResp > 0);
+            elseif strcmp(obj.patchContrast,'negative')
+                inds = find(LnResp <= 0);
+            end
+            xLoc = imageData.(fieldName).location(inds,1);
+            yLoc = imageData.(fieldName).location(inds,2);
+            subunitResp = imageData.(fieldName).SubunitModelResponse(inds);
+            LnResp = imageData.(fieldName).LnModelResponse(inds);
+            
+            %2) do patch sampling:
+            responseDifferences = subunitResp - LnResp;
+            if strcmp(obj.patchSampling,'random')
+                %get patch indices:
+                pullInds = randsample(1:length(xLoc),obj.numberOfAverages);
+            else strcmp(obj.patchSampling,'ranked')
+                %pull more than needed to account for empty bins at tail
+                [~, ~, bin] = histcounts(responseDifferences,1.5*obj.numberOfAverages);
+                populatedBins = unique(bin);
+                %pluck one patch from each bin
+                pullInds = arrayfun(@(b) find(b == bin,1),populatedBins);
+                %get patch indices:
+                pullInds = randsample(pullInds,obj.numberOfAverages);
+            end
+            obj.patchLocations(1,1:obj.numberOfAverages) = xLoc(pullInds); %in VH pixels
+            obj.patchLocations(2,1:obj.numberOfAverages) = yLoc(pullInds);
+            subunitResp = subunitResp(pullInds);
+            LnResp = LnResp(pullInds);
+            responseDifferences = subunitResp - LnResp;
+            
+%             figure(30); clf;
+%             subplot(211); hist(responseDifferences,100);
+%             subplot(212); plot(subunitResp,LnResp,'ko');
+%             title('On model responses')
+            
+            %get equivalent intensity values:
+            %   Get the model RF...
+            sigmaC = obj.rfSigmaCenter ./ 3.3; %microns -> VH pixels
+            RF = fspecial('gaussian',2.*[radX radY] + 1,sigmaC);
+
+            %   get the aperture to apply to the image...
+            %   set to 1 = values to be included (i.e. image is shown there)
+            [rr, cc] = meshgrid(1:(2*radX+1),1:(2*radY+1));
+            if obj.apertureDiameter > 0
+                apertureMatrix = sqrt((rr-radX).^2 + ...
+                    (cc-radY).^2) < (obj.apertureDiameter/2) ./ 3.3;
+                apertureMatrix = apertureMatrix';
+            else
+                apertureMatrix = ones(2.*[radX radY] + 1);
+            end
+            if strcmp(obj.linearIntegrationFunction,'gaussian center')
+                weightingFxn = apertureMatrix .* RF; %set to zero mean gray pixels
+            elseif strcmp(obj.linearIntegrationFunction,'uniform')
+                weightingFxn = apertureMatrix;
+            end
+            weightingFxn = weightingFxn ./ sum(weightingFxn(:)); %sum to one
+            
+            for ff = 1:obj.numberOfAverages
+                tempPatch = contrastImage(round(obj.patchLocations(1,ff)-radX):round(obj.patchLocations(1,ff)+radX),...
+                    round(obj.patchLocations(2,ff)-radY):round(obj.patchLocations(2,ff)+radY));
+                equivalentContrast = sum(sum(weightingFxn .* tempPatch));
+                obj.allEquivalentIntensityValues(ff) = obj.backgroundIntensity + ...
+                    equivalentContrast * obj.backgroundIntensity;
+            end
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+
+            %pull patch location and equivalent contrast:
+            obj.imagePatchIndex = floor(mod(obj.numEpochsCompleted/2,obj.noPatches) + 1);
+            evenInd = mod(obj.numEpochsCompleted,2);
+            if evenInd == 1 %even, show uniform linear equivalent intensity
+                obj.stimulusTag = 'intensity';
+            elseif evenInd == 0 %odd, show image
+                obj.stimulusTag = 'image';
+            end
+            
+            obj.currentPatchLocation(1) = obj.patchLocations(1,obj.imagePatchIndex); %in VH pixels
+            obj.currentPatchLocation(2) = obj.patchLocations(2,obj.imagePatchIndex);
+            obj.equivalentIntensity = obj.allEquivalentIntensityValues(obj.imagePatchIndex);
+            
+            device = obj.rig.getDevice(obj.amp);
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+            
+            %imagePatchMatrix is in VH pixels
+            %size of the stimulus on the prep:
+            stimSize = obj.rig.getDevice('Stage').getCanvasSize() .* ...
+                obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'); %um
+            stimSize_VHpix = stimSize ./ (3.3); %um / (um/pixel) -> pixel
+            radX = stimSize_VHpix(1) / 2; %boundaries for fixation draws depend on stimulus size
+            radY = stimSize_VHpix(2) / 2;
+            obj.imagePatchMatrix = obj.wholeImageMatrix(round(obj.currentPatchLocation(1)-radX):round(obj.currentPatchLocation(1)+radX),...
+                round(obj.currentPatchLocation(2)-radY):round(obj.currentPatchLocation(2)+radY));
+            obj.imagePatchMatrix = obj.imagePatchMatrix';
+%             figure(30); clf;
+%             imagesc(obj.imagePatchMatrix); colormap(gray); axis image; axis equal;
+
+            epoch.addParameter('currentStimSet', obj.currentStimSet);
+            epoch.addParameter('backgroundIntensity', obj.backgroundIntensity);
+            epoch.addParameter('imagePatchIndex', obj.imagePatchIndex);
+            epoch.addParameter('currentPatchLocation', obj.currentPatchLocation);
+            epoch.addParameter('equivalentIntensity', obj.equivalentIntensity);
+            epoch.addParameter('stimulusTag', obj.stimulusTag);
+        end
+        
+        function p = createPresentation(obj)
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p.setBackgroundColor(obj.backgroundIntensity);
+            
+            apertureDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.apertureDiameter);
+            centerOffsetPix = obj.rig.getDevice('Stage').um2pix(obj.centerOffset);
+
+            if strcmp(obj.stimulusTag,'image')
+                scene = stage.builtin.stimuli.Image(obj.imagePatchMatrix);
+                scene.size = canvasSize; %scale up to canvas size
+                scene.position = canvasSize/2 + centerOffsetPix;
+                % Use linear interpolation when scaling the image.
+                scene.setMinFunction(GL.LINEAR);
+                scene.setMagFunction(GL.LINEAR);
+                p.addStimulus(scene);
+                sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
+                    @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+                p.addController(sceneVisible);
+            elseif strcmp(obj.stimulusTag,'intensity')
+                scene = stage.builtin.stimuli.Rectangle();
+                scene.size = canvasSize;
+                scene.color = obj.equivalentIntensity;
+                scene.position = canvasSize/2 + centerOffsetPix;
+                p.addStimulus(scene);
+                sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
+                    @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+                p.addController(sceneVisible);
+            end
+            
+            if (obj.apertureDiameter > 0) %% Create aperture
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2 + centerOffsetPix;
+                aperture.color = obj.backgroundIntensity;
+                aperture.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(apertureDiameterPix/max(canvasSize), 1024); %circular aperture
+                aperture.setMask(mask);
+                p.addStimulus(aperture); %add aperture
+            end
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+
+    end
+    
+end
+
---
+++ +edu/+washington/+riekelab/+yu/+protocols/NatImageDynamicClampwithTimeShift.m
@@ -0,0 +1,256 @@
+classdef NatImageDynamicClampwithTimeShift < edu.washington.riekelab.protocols.RiekeLabProtocol
+    
+    properties
+        gExcMultiplier = 1
+        gInhMultiplier = 1
+        ConductanceSet = 'ONparasol_20160112Ec1'
+        ExcConductance = 'Image' %Constant, overridden by interleaves below
+        InhConductance = 'Image' %Constant, overridden by interleaves below
+        InterleaveImage = false; %{image-image / disc-disc / image-disc / disc-image}
+        InterleaveTonic = false; %{image-image / image-tonic / tonic-image}
+        ExcReversal = 10;
+        InhReversal = -70;
+        MeanConductanceTrace = true %Average conductance trace (over trials) or randomly choose individual trials
+        
+        amp                             % Input amplifier
+        numberOfAverages = uint16(5)    % Number of epochs
+        interpulseInterval = 0.2          % Duration between pulses (s)
+    end
+    
+    properties (Hidden)
+        ampType
+        ConductanceSetType = symphonyui.core.PropertyType('char', 'row',...
+            {'ONparasol_20160112Ec1','ONparasol_20160112Ec3','ONparasol_20160324Ec2',...
+            'OFFparasol_20160112Ec2','OFFparasol_20160324Ec4'})
+        ExcConductanceType = symphonyui.core.PropertyType('char', 'row', {'Image', 'Disc', 'Tonic'})
+        InhConductanceType = symphonyui.core.PropertyType('char', 'row', {'Image', 'Disc', 'Tonic'})
+        
+        stimSequence %struct with fields .exc and .inh (vals cell array of strings, image, disc, or tonic)
+        imageSequence %image ids
+        currentImageIndex
+        
+        currentConductanceStim %struct with fields .exc and .inh (vals str: image, disc, or tonic)
+        currentConductanceTrial %struct with fields .exc and .inh (vals int)
+        
+        resourcesDir
+    end
+    
+    properties (Hidden, Transient)
+        analysisFigure
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            obj.resourcesDir = 'C:\Users\Public\Documents\turner-package\resources\gClampStims\NIFgClampTraces\';
+            fileID = [obj.ConductanceSet,'.mat'];
+            load(fullfile(obj.resourcesDir, fileID));   
+            obj.imageSequence = res.imageIndex;
+            
+            %create conductance label sequence
+            if obj.InterleaveImage
+                %will cycle through to apply to both sequences
+                %to get all the appropriate pairings
+                obj.stimSequence.exc = {'Image', 'Image', 'Disc', 'Disc'};
+                obj.stimSequence.inh = {'Image', 'Disc','Image','Disc'};
+            elseif obj.InterleaveTonic
+               obj.stimSequence.exc = {'Image', 'Tonic', 'Image'};
+               obj.stimSequence.inh = {'Image', 'Image', 'Tonic'};
+            else
+                obj.stimSequence.exc = {obj.ExcConductance};
+                obj.stimSequence.inh = {obj.InhConductance};
+            end
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'groupBy',{'currentImageIndex'});
+            obj.showFigure('edu.washington.riekelab.turner.figures.DynamicClampFigure',...
+                obj.rig.getDevice(obj.amp), obj.rig.getDevice('Excitatory conductance'),...
+                obj.rig.getDevice('Inhibitory conductance'), obj.rig.getDevice('Injected current'),...
+                obj.ExcReversal, obj.InhReversal);
+            
+            % custom figure handler
+            if isempty(obj.analysisFigure) || ~isvalid(obj.analysisFigure)
+                obj.analysisFigure = obj.showFigure('symphonyui.builtin.figures.CustomFigure', @obj.NIFgClamp);
+                f = obj.analysisFigure.getFigureHandle();
+                set(f, 'Name', 'NIF gClamp');
+                obj.analysisFigure.userData.iiCount = 0;
+                obj.analysisFigure.userData.ddCount = 0;
+                obj.analysisFigure.userData.idCount = 0;
+                obj.analysisFigure.userData.diCount = 0;
+                obj.analysisFigure.userData.axesHandle = axes('Parent', f,...
+                    'FontName', get(f, 'DefaultUicontrolFontName'),...
+                    'FontSize', get(f, 'DefaultUicontrolFontSize'), ...
+                    'XTickMode', 'auto');
+                xlabel(obj.analysisFigure.userData.axesHandle, 'Image');
+                ylabel(obj.analysisFigure.userData.axesHandle, 'Disc');
+            end
+            
+            %set the backgrounds on the conductance commands
+            %0.05 V command per 1 nS conductance
+            excBackground = mean(mean(res.exc.image{1}(:,1:res.stimStart))) .* 0.05;
+            inhBackground = mean(mean(res.inh.image{1}(:,1:res.stimStart))) .* 0.05;
+            obj.rig.getDevice('Excitatory conductance').background = symphonyui.core.Measurement(excBackground, 'V');
+            obj.rig.getDevice('Inhibitory conductance').background = symphonyui.core.Measurement(inhBackground, 'V');
+        end
+        
+        function NIFgClamp(obj, ~, epoch) %online analysis function
+            response = epoch.getResponse(obj.rig.getDevice(obj.amp));
+            Vdata = response.getData();
+            sampleRate = response.sampleRate.quantityInBaseUnits;
+            
+            axesHandle = obj.analysisFigure.userData.axesHandle;
+            iiCount = obj.analysisFigure.userData.iiCount;
+            ddCount = obj.analysisFigure.userData.ddCount;
+            idCount = obj.analysisFigure.userData.idCount;
+            diCount = obj.analysisFigure.userData.diCount;
+            
+            fileID = [obj.ConductanceSet,'.mat'];
+            load(fullfile(obj.resourcesDir, fileID));
+            
+            %get iClamp spikes
+            threshold = -20; %mV - hardcoded for now, could be param?
+            spikesUp=edu.washington.riekelab.turner.utils.getThresCross(Vdata,threshold,1);
+            spikesDown=edu.washington.riekelab.turner.utils.getThresCross(Vdata,threshold,-1);
+            newSpikeCount = length(spikesUp(spikesUp>res.stimStart & spikesUp<res.stimEnd));
+            if strcmp(obj.currentConductanceStim.exc,'Image')
+                if strcmp(obj.currentConductanceStim.inh,'Image')
+                    iiCount(obj.currentImageIndex) = newSpikeCount;
+                elseif strcmp(obj.currentConductanceStim.inh,'Disc')
+                    idCount(obj.currentImageIndex) = newSpikeCount;
+                end
+            elseif strcmp(obj.currentConductanceStim.exc,'Disc')
+                if strcmp(obj.currentConductanceStim.inh,'Image')
+                    diCount(obj.currentImageIndex) = newSpikeCount;
+                elseif strcmp(obj.currentConductanceStim.inh,'Disc')
+                    ddCount(obj.currentImageIndex) = newSpikeCount;
+                end
+            end
+            
+            hd = line(res.spikes.image,res.spikes.disc,'Parent',axesHandle);
+            set(hd,'Color','g','LineStyle','none','Marker','x')
+            yUp = 1.3*max([res.spikes.image,res.spikes.disc]);
+            hu = line([0 yUp],[0 yUp],'Parent',axesHandle);
+            set(hu,'Color','k','LineStyle','--')
+            
+            if length(iiCount) == length(ddCount)
+                hd = line(iiCount,ddCount,'Parent',axesHandle);
+                set(hd,'Color','k','LineStyle','none','Marker','o')
+            end
+            
+            obj.analysisFigure.userData.axesHandle = axesHandle;
+            obj.analysisFigure.userData.iiCount = iiCount;
+            obj.analysisFigure.userData.ddCount = ddCount;
+            obj.analysisFigure.userData.idCount = idCount;
+            obj.analysisFigure.userData.diCount = diCount;
+        end
+        
+        function stim = createConductanceStimulus(obj,conductance)
+            %conductance is string: 'exc' or 'inh'
+            fileID = [obj.ConductanceSet,'.mat'];
+            load(fullfile(obj.resourcesDir, fileID));   
+            gen = symphonyui.builtin.stimuli.WaveformGenerator();
+            gen.sampleRate = obj.sampleRate;
+            gen.units = 'V';
+            if strcmp(obj.currentConductanceStim.(conductance),'Image')
+                if (obj.MeanConductanceTrace)
+                    obj.currentConductanceTrial.(conductance) = [];
+                    newConductanceTrace = mean(res.(conductance).image{obj.currentImageIndex});
+                else
+                    obj.currentConductanceTrial.(conductance) = ...
+                        randsample(1:size(res.(conductance).image{obj.currentImageIndex},1),1);
+                    newConductanceTrace = res.(conductance).image{obj.currentImageIndex}(obj.currentConductanceTrial.(conductance),:);
+                end
+            elseif strcmp(obj.currentConductanceStim.(conductance),'Disc')
+                if (obj.MeanConductanceTrace)
+                    obj.currentConductanceTrial.(conductance) = [];
+                    newConductanceTrace = mean(res.(conductance).disc{obj.currentImageIndex});
+                else
+                    obj.currentConductanceTrial.(conductance) = ...
+                        randsample(1:size(res.(conductance).disc{obj.currentImageIndex},1),1);
+                    newConductanceTrace = res.(conductance).disc{obj.currentImageIndex}(obj.currentConductanceTrial.(conductance),:);
+                end
+            elseif strcmp(obj.currentConductanceStim.(conductance),'Tonic')
+                if (obj.MeanConductanceTrace)
+                    obj.currentConductanceTrial.(conductance) = [];
+                    temp = mean(res.(conductance).image{obj.currentImageIndex});
+                    newConductanceTrace = mean(temp(1:res.stimStart)) .* ones(size(temp));
+                else
+                    obj.currentConductanceTrial.(conductance) = ...
+                        randsample(1:size(res.(conductance).image{obj.currentImageIndex},1),1);
+                    temp = res.(conductance).image{obj.currentImageIndex}(obj.currentConductanceTrial.(conductance),:);
+                    newConductanceTrace = mean(temp(1:res.stimStart)) .* ones(size(temp));
+                end     
+            end
+            if strcmp(conductance,'exc')
+                newConductanceTrace = obj.gExcMultiplier .* newConductanceTrace; %nS
+            elseif strcmp(conductance,'inh')
+                newConductanceTrace = obj.gInhMultiplier .* newConductanceTrace; %nS
+            end
+            %map conductance (nS) to DAC output (V) to match expectation of
+            %Arduino...
+            % 200 nS = 10 V, 1 nS = 0.05 V
+            mappedConductanceTrace = newConductanceTrace .* 0.05;
+            if any(mappedConductanceTrace > 10)
+                mappedConductanceTrace = zeros(1,length(mappedConductanceTrace));
+                error(['G_',conductance, ': voltage command out of range!'])
+            end
+            gen.waveshape = mappedConductanceTrace;
+            stim = gen.generate();
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, epoch);
+            
+            %get image from sequence
+            tempIndex = floor(obj.numEpochsCompleted / length(obj.stimSequence.exc));
+            drawIndex = mod(tempIndex, length(obj.imageSequence)) + 1;
+            % Randomize the image sequence order at the beginning of each image sequence
+            if drawIndex == 1
+                obj.imageSequence = randsample(obj.imageSequence, length(obj.imageSequence));
+            end
+            obj.currentImageIndex = drawIndex; %index of image in imageSequence of gClamp stim file
+            
+            % get conductance stim from sequence
+            drawIndex = mod(obj.numEpochsCompleted,length(obj.stimSequence.exc)) + 1;
+            obj.currentConductanceStim.exc = obj.stimSequence.exc{drawIndex};
+            obj.currentConductanceStim.inh = obj.stimSequence.inh{drawIndex};
+            
+            epoch.addStimulus(obj.rig.getDevice('Excitatory conductance'), obj.createConductanceStimulus('exc'));
+            epoch.addStimulus(obj.rig.getDevice('Inhibitory conductance'), obj.createConductanceStimulus('inh'));
+            epoch.addResponse(obj.rig.getDevice(obj.amp));
+            epoch.addResponse(obj.rig.getDevice('Injected current'));
+            
+            epoch.addParameter('currentGexcStim', obj.currentConductanceStim.exc);
+            epoch.addParameter('currentGinhStim', obj.currentConductanceStim.inh);
+            
+            epoch.addParameter('currentGexcTrial', obj.currentConductanceTrial.exc);
+            epoch.addParameter('currentGinhTrial', obj.currentConductanceTrial.inh);
+            epoch.addParameter('currentImageIndex', obj.currentImageIndex);
+        end
+        
+        function prepareInterval(obj, interval)
+            prepareInterval@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, interval);
+            
+            device = obj.rig.getDevice(obj.amp);
+            interval.addDirectCurrentStimulus(device, device.background, obj.interpulseInterval, obj.sampleRate);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+        
+    end
+    
+end
+
