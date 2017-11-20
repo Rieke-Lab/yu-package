93c1da420f6c05a9fbb6a3273d704741f9d5ddda
sadahanu <yuz04ster@gmail.com>
2017-11-20 13:15:14 -0800

    abandon gratings

--- +edu/+washington/+riekelab/+yu/+utils/createSkewGratings.m
+++ +edu/+washington/+riekelab/+yu/+utils/createSkewGratings.m
@@ -1,9 +1,12 @@
-function gratingMatrix = createSkewGratings( offset,barwidth,sz,pos_contrast,neg_contrast)
+function gratingMatrix = createSkewGratings( height, offset,unitbarwidth,sz,pos_ratio)
 %create skewed gratings with mean (offst)
 % to achieve sum(pos_conctrast) == neg(contrast) -> bar width is scaled
     grating = ones(sz,sz); %  create an image array
     center = floor(sz/2)+1; % center of grating is 
-    
-
+    wave = sin((linspace(1,sz,sz)-center)*pi/(unitbarwidth));
+    wave(wave>=0)=1;
+    wave(wave<0)=-1;
+    grating = ones(sz,1)*wave;
+    gratingMatrix = grating*height+offset;
 end
 
